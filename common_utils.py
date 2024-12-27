582695ba18af8e8d29b18c0d5977128e0cfe9e63
chiwuyin <chiwuyin68@gmail.com>
2024-12-27 11:55:38 +0800

    Create common_utils.py

---
+++ common_utils.py
@@ -0,0 +1,9 @@
+conda env export --no-builds > environment.yml
+conda env export --name my_env > my_env.yml
+conda env export > environment.yml
+conda env create -f environment.yml
+
+
+pip install -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple some-package
+python -m pip install --upgrade pip
+pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
