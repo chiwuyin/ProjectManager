c4330befb0aa0981e736299c9ca2e79fb1cbf861
chiwuyin <chiwuyin68@gmail.com>
2024-12-31 16:06:30 +0800

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,1 +1,9 @@
 https://www.promptingguide.ai/introduction/basics
+
+Instruction - a specific task or instruction you want the model to perform
+
+Context - external information or additional context that can steer the model to better responses
+
+Input Data - the input or question that we are interested to find a response for
+
+Output Indicator - the type or format of the output.
