03b95e3a1f34d8c544a0aeb4033ff0a436ad71a4
chiwuyin <chiwuyin68@gmail.com>
2025-01-14 12:03:40 +0800

    Create git.py

---
+++ git.py
@@ -0,0 +1,9 @@
+git init
+git remote add origin http://192.168.0.203/root/speech-text.git
+git add .
+git config --global user.email "zhiwu.yin@xxx"
+git config --global user.name "root"
+git commit -m "Initial commit"
+git push -u origin master  # 如果默认分支是 main，则使用 'main' 而不是 'master'
+git push http://root:xxx@192.168.0.203/root/speech-text.git
+git push http://root:xxx@192.168.0.203/root/speech-text.git
